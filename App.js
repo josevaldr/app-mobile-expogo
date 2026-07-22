538dc0b60972dd4b5ee37c0699393701ab62570f
José Valdir <josevaldir.candido@gmail.com>
2026-07-22 18:33:33 -0300

    Primeira versão do projeto

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Image } from 'react-native';
+import {
+  PaperProvider,
+  Appbar,
+  Card,
+  Text,
+} from 'react-native-paper';
+import styles from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Appbar.Header style={styles.header}>
+        <Appbar.Content title="Meu filme favorito" />
+      </Appbar.Header>
+
+      <ScrollView style={styles.tela}>
+        
+<Card style={styles.card}>
+
+  <Card.Cover source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR48sKyxTC3m7oGUQoxb9PI27o7KrJyDv9dDhwoibCmV5Q3bHk_vhxCN-Y&s=10" }} />
+
+  <Card.Content style={styles.conteudo}>
+
+    <Text style={styles.titulo}>
+      Constantine
+    </Text>
+
+    <Text style={styles.nota}>
+      2004 • Fantasia sobrenatural
+    </Text>
+
+    <Text style={styles.nota}>
+      ⭐ IMDb 7.0/10
+    </Text>
+
+    <Text style={styles.texto}>
+      John Constantine, um experiente ocultista e exorcista que já visitou o inferno, une forças com a policial cética Angela Dodson. Juntos, eles investigam o suicídio da irmã gêmea de Angela, mergulhando em um submundo sobrenatural povoado por anjos e demônios.
+    </Text>
+
+  </Card.Content>
+
+</Card>
+
+      </ScrollView>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,53 @@
+
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  tela: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+
+  
+  card: {
+    margin: 15,
+    backgroundColor: '#252525',
+    borderRadius: 22,
+    overflow: 'hidden',
+    elevation: 5,
+  },
+
+
+  imagem: {
+    width: '100%',
+    height: 400,
+  },
+
+  
+  titulo: {
+    color: '#F5C518',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 10,
+  },
+
+  nota: {
+    color: '#F5C518',
+    marginBottom: 15,
+  },
+
+  texto: {
+    color: '#FFFFFF',
+    lineHeight: 28,
+    fontSize: 17,
+  },
+
+  conteudo: {
+    padding: 1,
+  },
+
+
+    header: {
+      backgroundColor: '#F5C518',
+    },
+});
